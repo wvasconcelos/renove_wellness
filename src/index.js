5ccfa96f84c87de60ad3ccb24d7df4a92c34a59a
DELL-I7\willdv <willdavies@outlook.com>
2024-10-25 20:36:31 -0700

    First Commit to Firebase

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
+//require('dotenv').config();
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
